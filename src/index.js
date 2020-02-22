c42a99e30ab37ee4ea6f8b7d78627f1598abdbab
hugoroca <hugo.rock20@hotmail.com>
2020-02-22 00:40:07 -0500

    create index.js and configuration for first deploy

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import { ApolloServer, makeExecutableSchema } from "apollo-server"
+
+const typeDefs = `
+    type Hello {
+        message: String!
+    }
+
+    type Query {
+        sayHello(name: String!): Hello
+    }
+`
+
+const resolvers = {
+    Query: {
+        sayHello: (_, args) => {
+            return {
+                message: `Hello ${args.name || 'world'}`
+            }
+        }
+    }
+}
+
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+})
+
+const apolloServer = new ApolloServer({
+    schema
+})
+
+apolloServer.listen(5000).then(({ url }) => console.log(`Running on ${url}`))
