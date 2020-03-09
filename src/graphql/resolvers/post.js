73ab1b21c29ea9b2fabe12953513c1e667f556f4
hugoroca <hugo.rock20@hotmail.com>
2020-03-08 22:09:50 -0500

    tests

--- src/graphql/resolvers/post.js
+++ src/graphql/resolvers/post.js
@@ -1,22 +1,29 @@
 export default {
-    Query: {
-        getPosts: (parent, args, { models }) => {
-            return models.Post.findAll({
-                include: [{
-                    model: models.Tag,
-                    as: 'tags'
-                }]
-            })
-        }
-    },
-    Mutation: {
-        createPost: (parent, { input }, { models}) => {
-            return models.Post.create({ ...input }, {
-                include: [{
-                    model: models.Tag,
-                    as: 'tags'
-                }]
-            })
-        }
+  Query: {
+    getPosts: (parent, args, { models }) => {
+      return models.Post.findAll({
+        include: [
+          {
+            model: models.Tag,
+            as: 'tags'
+          }
+        ]
+      })
     }
+  },
+  Mutation: {
+    createPost: (parent, { input }, { models }) => {
+      return models.Post.create(
+        { ...input },
+        {
+          include: [
+            {
+              model: models.Tag,
+              as: 'tags'
+            }
+          ]
+        }
+      )
+    }
+  }
 }
