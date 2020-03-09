701170eecc65488853640aa62d206fa55f47ae24
hugoroca <hugo.rock20@hotmail.com>
2020-03-08 23:02:10 -0500

    finish test

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -9,7 +9,7 @@ import { encrypt, setBase64 } from './security'
 import { $security } from '../../config'
 import { isPasswordMatch } from './is'
 
-const createToken = async user => {
+export const createToken = async user => {
   const { id, username, password, email, privilege, active } = user
   const token = setBase64(`${encrypt($security().secretKey)}${password}`)
   const userData = {
